84250136d1a852c622e250f18318cc453245ec18
jmirandao1 <91306648+jmirandao1@users.noreply.github.com>
2021-09-25 05:56:00 -0600

    Add files via upload

---
+++ Actividades 2.3/Test/Apps/home/Apps/home/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Cliente, TipoCliente, Venta
+
+# Register your models here.
+admin.site.register(Cliente)
+admin.site.register(TipoCliente)
+admin.site.register(Venta)
+
+
